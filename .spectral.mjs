765ad8ea1f3e5f3a9c9c4e2ffbcb08d920872a74
Di Sheng Chen <352826256@qq.com>
2025-05-22 17:33:14 +0800

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4MjAwOQ/spectral.js?branch_slug=main&token=2404f9ae-bf1b-4a58-9ed5-2f0d5ea01257";
+export default { extends: ruleset };
